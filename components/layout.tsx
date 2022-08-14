ef8ba93c351e6c7be3f5536a79bc402437aca053
ksc <ksc@openkps.com>
2022-08-15 02:20:59 +0900

    전체 수정

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,9 +1,8 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
 import React from "react";
+import Link from "next/link";
 import { cls } from "../libs/utils";
+import { useRouter } from "next/router";
 
-// typescript에서만 사용가능.
 interface LayoutProps {
   title?: string;
   canGoBack?: boolean;
@@ -21,7 +20,6 @@ export default function Layout({
   const onClick = () => {
     router.back();
   };
-
   return (
     <div>
       <div className="bg-white w-full h-12 max-w-xl justify-center text-lg px-10 font-medium  fixed text-gray-800 border-b top-0  flex items-center">
@@ -49,10 +47,9 @@ export default function Layout({
       </div>
       <div className={cls("pt-12", hasTabBar ? "pb-24" : "")}>{children}</div>
       {hasTabBar ? (
-        <nav className="max-w-xl bg-white text-gray-700 border-t fixed bottom-0 w-full px-10 pb-5 pt-3 flex justify-between text-xs">
-          {/* https://heroicons.com/ 에서 Home 검색 아이콘 */}
+        <nav className="bg-white max-w-xl text-gray-700 border-t fixed bottom-0 w-full px-10 pb-5 pt-3 flex justify-between text-xs">
           <Link href="/">
-          <a
+            <a
               className={cls(
                 "flex flex-col items-center space-y-2 ",
                 router.pathname === "/"
@@ -61,25 +58,24 @@ export default function Layout({
               )}
             >
               <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
+                className="w-6 h-6"
                 fill="none"
-                viewBox="0 0 24 24"
                 stroke="currentColor"
-                strokeWidth={2}
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
               >
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                />
+                ></path>
               </svg>
               <span>홈</span>
             </a>
           </Link>
-          {/* https://heroicons.com/ 에서 newspaper 검색 아이콘 */}
           <Link href="/community">
-          <a
+            <a
               className={cls(
                 "flex flex-col items-center space-y-2 ",
                 router.pathname === "/community"
@@ -88,25 +84,24 @@ export default function Layout({
               )}
             >
               <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
+                className="w-6 h-6"
                 fill="none"
-                viewBox="0 0 24 24"
                 stroke="currentColor"
-                strokeWidth={2}
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
               >
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"
-                />
+                ></path>
               </svg>
               <span>동네생활</span>
             </a>
           </Link>
-          {/* https://heroicons.com/ 에서 chat 검색 아이콘 */}
           <Link href="/chats">
-          <a
+            <a
               className={cls(
                 "flex flex-col items-center space-y-2 ",
                 router.pathname === "/chats"
@@ -115,25 +110,24 @@ export default function Layout({
               )}
             >
               <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
+                className="w-6 h-6"
                 fill="none"
-                viewBox="0 0 24 24"
                 stroke="currentColor"
-                strokeWidth={2}
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
               >
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-                />
+                ></path>
               </svg>
               <span>채팅</span>
             </a>
           </Link>
-          {/* https://heroicons.com/ 에서 video-camera 검색 아이콘 */}
           <Link href="/live">
-          <a
+            <a
               className={cls(
                 "flex flex-col items-center space-y-2 ",
                 router.pathname === "/live"
@@ -142,25 +136,24 @@ export default function Layout({
               )}
             >
               <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
+                className="w-6 h-6"
                 fill="none"
-                viewBox="0 0 24 24"
                 stroke="currentColor"
-                strokeWidth={2}
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
               >
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
-                />
+                ></path>
               </svg>
               <span>라이브</span>
             </a>
           </Link>
-          {/* https://heroicons.com/ 에서 user 검색 아이콘 */}
           <Link href="/profile">
-          <a
+            <a
               className={cls(
                 "flex flex-col items-center space-y-2 ",
                 router.pathname === "/profile"
@@ -169,18 +162,18 @@ export default function Layout({
               )}
             >
               <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
+                className="w-6 h-6"
                 fill="none"
-                viewBox="0 0 24 24"
                 stroke="currentColor"
-                strokeWidth={2}
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
               >
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-                />
+                ></path>
               </svg>
               <span>나의 캐럿</span>
             </a>
--- pages/community/[id].tsx
+++ pages/community/[id].tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Layout from "../../components/layout";
+import TextArea from "../../components/textarea";
 
 const CommunityPostDetail: NextPage = () => {
   return (
@@ -8,7 +9,7 @@ const CommunityPostDetail: NextPage = () => {
         <span className="inline-flex my-3 ml-4 items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
           동네질문
         </span>
-        <div className="flex mb-3 px-4 cursor-pointer pb-3 border-b items-center space-x-3">
+        <div className="flex mb-3 px-4 cursor-pointer pb-3  border-b items-center space-x-3">
           <div className="w-10 h-10 rounded-full bg-slate-300" />
           <div>
             <p className="text-sm font-medium text-gray-700">Steve Jebs</p>
@@ -74,10 +75,10 @@ const CommunityPostDetail: NextPage = () => {
           </div>
         </div>
         <div className="px-4">
-          <textarea
-            className="mt-1 shadow-sm w-full focus:ring-orange-500 rounded-md border-gray-300 focus:border-orange-500 "
-            rows={4}
+          <TextArea
+            name="description"
             placeholder="Answer this question!"
+            required
           />
           <button className="mt-2 w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none ">
             Reply
